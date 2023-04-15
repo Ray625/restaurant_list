a25771783896d53e324bf0325b577054a46faf6e
Ray625 <wdvgy32@gmail.com>
2023-04-15 21:55:09 +0800

    feat: add restaurant seeder

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurant.js')
+const restaurants = require('../../restaurant.json')
+const restaurantList = restaurants.results
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+  Restaurant.create(restaurantList)
+    .then(() => console.log('done'))
+    .catch(error => console.log(error))
+})
