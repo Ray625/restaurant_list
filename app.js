4b79d8f8f66172afc350c729d2b9d588b6798dbe
Ray625 <wdvgy32@gmail.com>
2023-04-15 16:32:27 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const restaurants = require('./restaurant.json')
 const app = express()
 const port = 3000
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -11,14 +29,15 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   const restaurantList = restaurants.results
-  res.render('index', { restaurant: restaurantList })
+  res.render('index', { restaurants: restaurantList })
 })
 
 app.get('/search', (req, res) => {
-  const restaurant = restaurants.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(req.query.keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(req.query.keyword.toLowerCase())
+  const keyword = req.query.keyword
+  const restaurantList = restaurants.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
   })
-  res.render('index', { restaurant, keyword: req.query.keyword })
+  res.render('index', { restaurants: restaurantList, keyword })
 })
 
 app.get('/restaurants/:id', (req, res) => {
