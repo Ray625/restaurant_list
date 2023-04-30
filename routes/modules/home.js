102cdf95667382cb958db232f597c37ccdd30183
Ray625 <wdvgy32@gmail.com>
2023-04-30 14:53:55 +0800

    feat: add relations between User and Restaurant models

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
 router.get('/', (req, res) => {
-  Restaurant.find()
+  const userId = req.user._id
+  Restaurant.find({ userId })
     .lean()
     .sort({ name: 'asc' })
     .then(restaurants => res.render('index', { restaurants }))
@@ -30,9 +31,14 @@ router.get('/search', (req, res) => {
   }
 
   Restaurant.find({
-    $or: [
-      { name: { $regex: new RegExp(keyword, 'i') } },
-      { category: { $regex: new RegExp(keyword, 'i') } },
+    $and: [
+      { userId: userId },
+      {
+        $or: [
+          { name: { $regex: new RegExp(keyword, 'i') } },
+          { category: { $regex: new RegExp(keyword, 'i') } },
+        ]
+      }
     ]
   })
     .lean()
--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -8,31 +8,35 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
   const restaurant = req.body
+  restaurant.userId = req.user._id
   return Restaurant.create(restaurant)     // 存入資料庫
     .then(() => res.redirect('/')) // 新增完成後導回首頁
     .catch(error => console.log(error))
 })
 
 router.get('/:id', (req, res) => {
-  const id = req.params.id
-  Restaurant.findById(id)
+  const userId = req.user._id
+  const id = req.params._id
+  Restaurant.findOne({ userId, id })
     .lean()
     .then(restaurant => res.render('show', { restaurant }))
     .catch(error => console.log(error))
 })
 
 router.get('/:id/edit', (req, res) => {
-  const id = req.params.id
-  Restaurant.findById(id)
+  const userId = req.user._id
+  const id = req.params._id
+  Restaurant.findOne({ userId, id })
     .lean()
     .then(restaurant => res.render('edit', { restaurant }))
     .catch(error => console.log(error))
 })
 
 router.put('/:id', (req, res) => {
-  const id = req.params.id
+  const userId = req.user._id
+  const id = req.params._id
   const restaurantNewData = req.body
-  Restaurant.findById(id)
+  Restaurant.findOne({ userId, id })
     .then(restaurant => {
       Object.assign(restaurant, restaurantNewData)
       return restaurant.save()
@@ -42,8 +46,9 @@ router.put('/:id', (req, res) => {
 })
 
 router.delete('/:id', (req, res) => {
-  const id = req.params.id
-  Restaurant.findById(id)
+  const userId = req.user._id
+  const id = req.params._id
+  Restaurant.findOne({ userId, id })
     .then(restaurant => restaurant.remove())
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
