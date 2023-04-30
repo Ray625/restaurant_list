bae19b9b4f4b9c3d044ddb8f6f0feaefc3e7456e
Ray625 <wdvgy32@gmail.com>
2023-04-30 11:08:52 +0800

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -12,6 +12,23 @@ router.get('/', (req, res) => {
 
 router.get('/search', (req, res) => {
   const keyword = req.query.keyword
+  const sortMethod = req.query.sortMethod
+  let sortIndex = { name: 'asc' }
+  switch (sortMethod) {
+    case 'Z -> A':
+      sortIndex = { name: 'desc' };
+      break;
+    case '類別':
+      sortIndex = { category: 'asc' };
+      break;
+    case '地區':
+      sortIndex = { location: 'asc' };
+      break;
+    default:
+      sortIndex = { name: 'asc' };
+      break;
+  }
+
   Restaurant.find({
     $or: [
       { name: { $regex: new RegExp(keyword, 'i') } },
@@ -19,7 +36,8 @@ router.get('/search', (req, res) => {
     ]
   })
     .lean()
-    .then(restaurants => res.render('index', { restaurants, keyword }))
+    .sort(sortIndex)
+    .then(restaurants => res.render('index', { restaurants, keyword, sortMethod }))
     .catch(error => console.log(error))
 })
 
